7efc03967c43de5294d43de5a2f7a271f910fadb
Rachel_Ren <Rachel_Ren@epam.com>
2018-06-04 17:50:40 +0800

    Rachel edited

--- testcase/test_pim/test_add_dependents_rachel.py
+++ testcase/test_pim/test_add_dependents_rachel.py
@@ -17,7 +17,7 @@ class TestAddDependents(unittest.TestCase):
     passwd = config.PASSWORD
     browser = config.BROWSER
 
-    # Create a Dependents
+    # Create a Dependent
     name = "Dependents"
     name1 = "Dependents1"
     name2 = "Dependents2"
@@ -26,7 +26,13 @@ class TestAddDependents(unittest.TestCase):
     relationship = "note"
     dateOfBirth = "1990-05-09"
     first_name = 'rachel'
-    last_name = 'test2'
+    last_name = 'test9'
+
+    # create an attachment
+    attachment = "test.docx"
+    comment = "Added attachment"
+    new_attachment = "testedit.docx"
+    new_comment = "Edited attachment"
 
     @classmethod
     def setUpClass(cls):
@@ -35,26 +41,50 @@ class TestAddDependents(unittest.TestCase):
         cls.login.open_browser(config.LOGIN_URL)
         cls.login.login(config.USER_NAME, config.PASSWORD)
         cls.dep = Dependents(cls.driver)
+        cls.dep.open_dependents_page_via_creating_emp(cls.first_name, cls.last_name)
+        # self.dep.open_dependents_page_via_editing_emp(self.first_name, self.last_name)
 
     def test_add_dependents_after_creating_emp_type_child(self):
-        self.dep.open_dependents_page_via_creating_emp(self.first_name, self.last_name)
         self.dep.add_dependents_without_specify(self.name, self.relationshipType_child, self.dateOfBirth)
+        self.dep.delete_the_first_dependents()
 
     def test_add_dependents_after_creating_emp_type_other(self):
         self.dep.add_dependents_with_specify(self.name, self.relationshipType_other, self.relationship, self.dateOfBirth)
+        self.dep.delete_the_first_dependents()
 
     def test_edit_the_first_dependents_save(self):
+        self.dep.add_dependents_without_specify(self.name, self.relationshipType_child, self.dateOfBirth)
         self.dep.edit_the_first_dependents_save(self.name1)
+        self.dep.delete_the_first_dependents()
 
     def test_edit_the_first_dependents_cancel(self):
-        self.dep.edit_the_first_dependents_cancle(self.name2)
+        self.dep.add_dependents_without_specify(self.name, self.relationshipType_child, self.dateOfBirth)
+        self.dep.edit_the_first_dependents_cancel(self.name2)
+        self.dep.delete_the_first_dependents()
 
     def test_delete_the_first_dependents(self):
+        self.dep.add_dependents_without_specify(self.name, self.relationshipType_child, self.dateOfBirth)
         self.dep.delete_the_first_dependents()
+
+    def test_add_attachments_save(self):
+        self.dep.add_attachments_upload(self.attachment, self.comment)
+        self.dep.edit_the_first_attachments(self.new_attachment, self.new_comment)
+
+    def test_add_attachments_cancel(self):
+        self.dep.add_attachments_cancel(self.attachment, self.comment)
+
+    def test_edit_the_first_attachment(self):
+        self.dep.add_attachments_upload(self.attachment, self.comment)
+        self.dep.edit_the_first_attachments(self.new_attachment, self.new_comment)
+        self.dep.delete_the_first_attachments()
+
+    def test_delete_the_first_attachments(self):
+        self.dep.add_attachments_upload(self.attachment, self.comment)
+        self.dep.delete_the_first_attachments()
 
     @classmethod
     def tearDownClass(cls):
         cls.dep.quit_browser()
 
-    if __name__ == "__main__":
-        unittest.main()
+if __name__ == "__main__":
+    unittest.main()
