74fbd2254136d4e06ea1809cdc6a1fd00f0cc4e0
anne <anne_tang@epam.com>
2018-06-04 16:39:15 +0800

    update

--- pageobjects/admin/config/email_sub.py
+++ pageobjects/admin/config/email_sub.py
@@ -1,5 +1,63 @@
+# -*- coding: utf-8 -*-
 '''
-Created on 2018Äê4ÔÂ26ÈÕ
+Created on 2018ï¿½ï¿½4ï¿½ï¿½26ï¿½ï¿½
 
 @author: Angelia_Yao
 '''
+from pageobjects.admin.admin import Admin
+from lib.log import Log
+
+
+class EmailSub(Admin):
+    """
+    Notification components
+    """
+    edit_btn = ('id', 'btnEdit')
+    save_btn = ('id', 'btnSave')
+    lea_app = ('xpath', '//a[text()="Leave Applications"]')
+    lea_app_box = ('id', 'ohrmList_chkSelectRecord_1')
+    lea_ass = ('xpath', '//a[text()="Leave Assignments"]')
+    lea_ass_box = ('id','ohrmList_chkSelectRecord_2' )
+    lea_approvals = ('xpath', '//a[text()="Leave Approvals"]')
+    lea_approvals_box = ('id','ohrmList_chkSelectRecord_3' )
+    lea_cancel = ('xpath', '//a[text()="Leave Cancellations"]')
+    lea_cancel_box = ('id','ohrmList_chkSelectRecord_4' )
+    lea_rejections = ('xpath', '//a[text()="Leave Rejections"]')
+    lea_rejections_box = ('id','ohrmList_chkSelectRecord_5' )
+    checkboxes = ('xpath', '//input[@type="checkbox"]')
+
+    def __init__(self, browser):
+        super(Admin, self).__init__(browser)
+
+    # def switch_menu(self):
+    #     """
+    #     Back to Email Configuration page
+    #     """
+    #     self.click_menu("Admin")
+    #     self.click_menu("Configuration")
+    #     self.click_menu("Email Subscriptions")
+
+    def enable_notification(self):
+        self.click(self.edit_btn)
+        self.click(self.lea_app_box)
+        self.click(self.lea_ass_box)
+        self.click(self.lea_approvals_box)
+        self.click(self.lea_cancel_box)
+        self.click(self.lea_rejections_box)
+        self.click(self.save_btn)
+        self.check_element_selected(self.lea_app_box)
+        self.check_element_selected(self.lea_ass_box)
+        self.check_element_selected(self.lea_approvals_box)
+        self.check_element_selected(self.lea_cancel_box)
+        self.check_element_selected(self.lea_rejections_box)
+
+    def disable_notification(self):
+        self.click(self.edit_btn)
+        self.click(self.lea_app_box)
+        self.click(self.lea_ass_box)
+        self.click(self.lea_approvals_box)
+        self.click(self.lea_cancel_box)
+        self.click(self.lea_rejections_box)
+        self.click(self.save_btn)
+
+
