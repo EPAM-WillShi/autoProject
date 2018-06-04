8c5edf31728993893c39d6fffa002dee9c0c918e
linda <linda_zang@epam.com>
2018-06-04 17:34:20 +0800

    Updated file path

--- config/config.py
+++ config/config.py
@@ -5,7 +5,7 @@ LOG_LEVEL = 'debug'  # debug,info,warn,error,critical
 LOG_FILEMODE = 'w'  # 'a','r', 'w', 'r+', 'w'
 
 # Setting the uploaded file path.
-UPLOAD_PATH = 'testfiles\\'
+UPLOAD_PATH = 'testfiles'
 
 # BROWSER define the browser type (chrome, ie, firefox) for testing
 # BROWSER = "firefox"
