2101d4086e6c4bb18b61d2827363455e430e25ef
anne <anne_tang@epam.com>
2018-06-04 12:57:16 +0800

    upload edit email configuration

--- testcase/test_admin/test_email_configuration.py
+++ testcase/test_admin/test_email_configuration.py
@@ -6,7 +6,7 @@ from pageobjects.admin.config.email_config import EmailConfig
 from pageobjects.login import Login
 
 
-class Testsaveemailconfig(unittest.TestCase):
+class Testemailconfig(unittest.TestCase):
     """
     Test Reset Button
     """
@@ -14,13 +14,15 @@ class Testsaveemailconfig(unittest.TestCase):
     ptitle = "Mail Configuration"
     sentas = "287326935@qq.com"
     sendmothod = "SMTP"
+    sendmothod1 = "Sendmail"
+    sendmothod2 = "sendmail"
+    path = "D:\Anne"
     smtphost = "smtp.qq.com"
     smtpport = "587"
     smtpuser = "287326935@qq.com"
     smtppsd = "49ba59abbe56e057"
     testemail = "anne_tang@epam.com"
     attkey = "value"
-
 
     @classmethod
     def setUpClass(cls):
@@ -29,7 +31,6 @@ class Testsaveemailconfig(unittest.TestCase):
         cls.login.open_browser(config.LOGIN_URL)
         cls.login.login(config.USER_NAME, config.PASSWORD)
         cls.emailcof = EmailConfig(cls.driver)
-        # cls.bps=BasePage(cls.driver)
         cls.emailcof.max_browser()
 
     def test_save_email_config(self):
@@ -39,14 +40,13 @@ class Testsaveemailconfig(unittest.TestCase):
         self.emailcof.enter_smtpinfo(self.smtphost, self.smtpport,self.smtpuser, self.smtppsd)
         self.emailcof.enter_testemail(self.testemail)
         self.emailcof.check_info(self.sentas, self.smtphost, self.smtpport,self.smtpuser, self.attkey)
-
-
-
+    # def test_edit_email_config(self):
+    #     self.emailcof.switch_menu()
+        self.emailcof.edit_email(self.sendmothod1, self.sendmothod2, self.path, self.attkey)
 
     @classmethod
     def tearDownClass(cls):
         cls.emailcof.quit_browser()
-
 
 if __name__ == "__main__":
     unittest.main()
