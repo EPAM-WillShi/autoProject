f43fa67b6d1b19297e32e3969ae5f9664e2c44a3
Dora Zhu <Dora_Zhu@epam.com>
2018-06-05 13:34:24 +0800

    add test case for PIM memberships

---
+++ pageobjects/pim/emp_memberships.py
@@ -0,0 +1,108 @@
+# -*- coding: utf-8 -*-
+
+from lib.log import Log
+from decimal import *
+from pageobjects.pim.employee_list import EmployeeList
+from pageobjects.admin.qual.memberships import QualityMemberships
+
+class Memberships(EmployeeList):
+    """
+    Employee list - Job
+    """
+    mem_flag = ('XPATH', '//h1[text()="Assigned Memberships"]')
+    add_btn = ('ID', 'btnAddMembershipDetail')
+    add_mem_flag = ('ID', 'membershipHeading')
+    assigned_flag = ('XPATH', '//h1[contains(text(), "Memberships")]')
+
+    mem_ship = ('ID', 'membership_membership')
+    sub_paid_by = ('ID', 'membership_subscriptionPaidBy')
+    sub_amount = ('ID','membership_subscriptionAmount')
+    currency = ('ID', 'membership_currency')
+    sub_com_date = ('ID', 'membership_subscriptionCommenceDate')
+    sub_renew_date = ('ID', 'membership_subscriptionRenewalDate')
+    save_btn = ('ID', 'btnSaveMembership')
+    message = ('XPATH', '//*[@class="message success fadable"]')
+
+    chart_mem_ship = '//td[2]/a[contains(text(),"{}")]'
+    subscription_paid_by = '//td[2]/a[contains(text(),"{}")]/../../td[3]'
+    subscription_amount = '//td[2]/a[contains(text(),"{}")]/../../td[4]'
+    subscription_currency = '//td[2]/a[contains(text(),"{}")]/../../td[5]'
+    subscription_commence_date = '//td[2]/a[contains(text(),"{}")]/../../td[6]'
+    subscription_renew_data = '//td[2]/a[contains(text(),"{}")]/../../td[7]'
+    chk_box = '//td[2]/a[contains(text(),"{}")]/../../td[1]//input'
+    del_btn = ('ID', 'delMemsBtn')
+
+    def __int__(self, browser):
+        super(Memberships, self).__int__(browser)
+        self.switch_main_menu("PIM")
+        self.click_menu("Employee List")
+        Log.info("Arrive Employee List page")
+
+    def open_memberships_page_via_creating_emp(self, first_name, last_name):
+        """
+        Open Membership page under Employee List by creating a new employee
+        """
+        self.add_employee(first_name, last_name)
+        self.switch_employee_detail_page("Memberships")
+        page_ele = self.get_element(self.mem_flag)
+        if page_ele is not None:
+            Log.info("Arrive Membership_page")
+
+    def add_membership(self, membership, pay, amount, currency, com_date, renew_date, abb_currency):
+        """
+        Add membership
+        """
+        self.click(self.add_btn)
+        assert self.get_element(self.add_mem_flag).is_displayed()
+        # assert "Add Membership" == self.get_element_text(self.add_mem_flag).encode("utf-8")
+        self.set_combox_value(membership, self.mem_ship)
+        self.set_combox_value(pay, self.sub_paid_by)
+        self.input_text(amount, self.sub_amount)
+        self.set_combox_value(currency, self.currency)
+        self.input_text(com_date, self.sub_com_date)
+        self.input_text(renew_date, self.sub_renew_date)
+        self.click(self.save_btn)
+        assert self.get_element_text(self.assigned_flag).encode("utf-8") == "Assigned Memberships"
+        assert self.get_element(("XPATH", self.chart_mem_ship.format(membership))).is_displayed()
+        assert self.get_element_text(("XPATH", self.subscription_paid_by.format(membership))) == pay
+        sub_amount = self.get_element_text(("XPATH", self.subscription_amount.format(membership))).encode("utf-8").replace(",", "")
+        assert Decimal(sub_amount) == Decimal(amount)
+        assert self.get_element_text(("XPATH", self.subscription_currency.format(membership))) == abb_currency
+        assert self.get_element_text(("XPATH", self.subscription_commence_date.format(membership))) == com_date
+        assert self.get_element_text(("XPATH", self.subscription_renew_data.format(membership))) == renew_date
+
+    def edit_membership(self, membership, modified_pay, modified_amount):
+        """
+        Edit membership
+        """
+        chat_mem = ('LINK_TEXT', membership)
+        self.click(chat_mem)
+        assert self.get_element_text(self.add_mem_flag).encode("utf-8") == "Edit Membership"
+        self.set_combox_value(modified_pay, self.sub_paid_by)
+        self.input_text(modified_amount, self.sub_amount)
+        self.click(self.save_btn)
+        assert self.get_element_text(("XPATH", self.subscription_paid_by.format(membership))) == modified_pay
+        sub_amount = self.get_element_text(("XPATH", self.subscription_amount.format(membership))).encode("utf-8").replace(",", "")
+        assert Decimal(sub_amount) == Decimal(modified_amount)
+
+    def delete_membership(self, membership):
+        """
+        Delete membership
+        """
+        checkbox = ("XPATH", self.chk_box.format(membership))
+        self.click(checkbox)
+        self.click(self.del_btn)
+
+    def verify_membership_success(self, return_message):
+        """
+        Assert the result of current operation
+
+        """
+        assert return_message in self.get_element_text(self.message)
+        Log.info(return_message)
+
+
+
+
+
+
---
+++ testcase/test_pim/test_edit_emp_memberships.py
@@ -0,0 +1,75 @@
+# coding:utf-8
+import unittest
+from config import config
+from com import utils
+from pageobjects.login import Login
+from pageobjects.pim.emp_memberships import Memberships
+from pageobjects.admin.qual.memberships import QualityMemberships
+
+
+class TestMemberships(unittest.TestCase):
+    """
+    Test PIM Add Employee page functions
+    """
+    # Login
+    login_url = config.LOGIN_URL
+    username = config.USER_NAME
+    passwd = config.PASSWORD
+    browser = config.BROWSER
+
+    # Create an employee
+    first_name = "dora1"
+    last_name = "test"
+
+    # Create a membership
+    membership = "membership_dora_test"
+
+    pay = "Company"
+    amount = "110"
+    currency = "Chinese Yuan Renminbi"
+    abb_currency = "CNY"  # Abbreviation of currency
+    com_date = "2018-06-02"
+    renew_date = "2020-06-02"
+
+    modified_pay = "Individual"
+    modified_amount = "1000"
+
+    @classmethod
+    def setUpClass(cls):
+        cls.driver = utils.get_browser_driver(cls.browser)
+        cls.login = Login(cls.driver)
+        cls.login.open_browser(config.LOGIN_URL)
+        cls.login.login(config.USER_NAME, config.PASSWORD)
+
+        cls.qua_memberships = QualityMemberships(cls.driver)
+        cls.qua_memberships.add_quality_membership(cls.membership)
+
+        cls.memberships = Memberships(cls.driver)
+        cls.memberships.open_memberships_page_via_creating_emp(cls.first_name, cls.last_name)
+
+    def test_case1_add_membership(self):
+        """
+        test_case1_add membership information for employee
+        """
+        self.memberships.add_membership(self.membership, self.pay, self.amount, self.currency,
+                                        self.com_date, self.renew_date, self.abb_currency)
+        self.memberships.verify_membership_success("Successfully Saved")
+
+    def test_case2_edit_membership(self):
+        """
+         test_case2_edit membership information for employee
+         """
+        self.memberships.edit_membership(self.membership, self.modified_pay, self.modified_amount)
+        self.memberships.verify_membership_success("Successfully Saved")
+
+    def test_case3_delete_membership(self):
+        """
+         test_case3_delete membership
+         """
+        self.memberships.delete_membership(self.membership)
+        self.memberships.verify_membership_success("Successfully Deleted")
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.memberships.quit_browser()
+
