8264119e4b843982e5de41cd29df8ed7ee13b06f
Rachel_Ren <Rachel_Ren@epam.com>
2018-06-08 15:35:02 +0800

    add choice value

--- pageobjects/leave/configure/work_week.py
+++ pageobjects/leave/configure/work_week.py
@@ -6,6 +6,7 @@ Created on 2018/4/17
 
 from lib.log import Log
 from pageobjects.leave.leave import Leave
+import random
 
 
 class WorkWeek(Leave):
@@ -16,6 +17,22 @@ class WorkWeek(Leave):
     monday_dropdown = ('id', 'WorkWeek_day_length_Monday')
     tuesday_dropdown = ('id', 'WorkWeek_day_length_Tuesday')
     message = ('xpath', "//div[contains(@class,'success')]")
+    """
+    Add components by Rachel
+    """
+    wednesday_dropdown = ('id', 'WorkWeek_day_length_Wednesday')
+    thursday_dropdown = ('id', 'WorkWeek_day_length_Thursday')
+    friday_dropdown = ('id', 'WorkWeek_day_length_Friday')
+    saturday_dropdown = ('id', 'WorkWeek_day_length_Saturday')
+    sunday_dropdown = ('id', 'WorkWeek_day_length_Sunday')
+    day_value = ['Full Day', 'Half Day', 'Non-working Day']
+    monday_choice = random.choice(day_value)
+    tuesday_choice = random.choice(day_value)
+    wednesday_choice = random.choice(day_value)
+    thursday_choice = random.choice(day_value)
+    friday_choice = random.choice(day_value)
+    saturday_choice = random.choice(day_value)
+    sunday_choice = random.choice(day_value)
 
     def __init__(self, browser):
         super(WorkWeek, self).__init__(browser)
@@ -23,8 +40,13 @@ class WorkWeek(Leave):
         self.click_menu("Work Week")
 
     def select_month(self):
-        self.set_combox_value("Non-working Day", self.tuesday_dropdown)
-        self.set_combox_value("Non-working Day", self.monday_dropdown)
+        self.set_combox_value(self.monday_choice, self.monday_dropdown)
+        self.set_combox_value(self.tuesday_choice, self.tuesday_dropdown)
+        self.set_combox_value(self.wednesday_choice, self.wednesday_dropdown)
+        self.set_combox_value(self.thursday_choice, self.thursday_dropdown)
+        self.set_combox_value(self.friday_choice, self.friday_dropdown)
+        self.set_combox_value(self.saturday_choice, self.saturday_dropdown)
+        self.set_combox_value(self.sunday_choice, self.sunday_dropdown)
 
     def save_workweek(self):
         self.click(self.edit_button)
