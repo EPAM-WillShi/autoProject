331bc0a93e70311bf608bc421ebcd370a6d7d80d
anne <anne_tang@epam.com>
2018-06-15 14:21:42 +0800

    update

--- pageobjects/admin/config/email_sub.py
+++ pageobjects/admin/config/email_sub.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
-'''
+"""
 Created on 2018��4��26��
 
 @author: Angelia_Yao
-'''
+"""
+
 from pageobjects.admin.admin import Admin
 from lib.log import Log
 
@@ -12,22 +13,37 @@ class EmailSub(Admin):
     """
     Notification components
     """
+    sub_title = ('xpath', '//h1[text()="Subscribers : Leave Applications"]')
+    add_subtitle = ('xpath', '//h1[@id="subscriberHeading"]')
     edit_btn = ('id', 'btnEdit')
     save_btn = ('id', 'btnSave')
     lea_app = ('xpath', '//a[text()="Leave Applications"]')
     lea_app_box = ('id', 'ohrmList_chkSelectRecord_1')
     lea_ass = ('xpath', '//a[text()="Leave Assignments"]')
-    lea_ass_box = ('id','ohrmList_chkSelectRecord_2' )
+    lea_ass_box = ('id', 'ohrmList_chkSelectRecord_2')
     lea_approvals = ('xpath', '//a[text()="Leave Approvals"]')
-    lea_approvals_box = ('id','ohrmList_chkSelectRecord_3' )
+    lea_approvals_box = ('id', 'ohrmList_chkSelectRecord_3')
     lea_cancel = ('xpath', '//a[text()="Leave Cancellations"]')
-    lea_cancel_box = ('id','ohrmList_chkSelectRecord_4' )
+    lea_cancel_box = ('id', 'ohrmList_chkSelectRecord_4')
     lea_rejections = ('xpath', '//a[text()="Leave Rejections"]')
-    lea_rejections_box = ('id','ohrmList_chkSelectRecord_5' )
+    lea_rejections_box = ('id', 'ohrmList_chkSelectRecord_5')
     checkboxes = ('xpath', '//input[@type="checkbox"]')
+    sub_addbtn = ('id', 'btnAdd')
+    sub_delbtn = ('id', 'btnDelete')
+    sub_backbtn = ('id', 'btnBack')
+    sub_name = ('id', 'subscriber_name')
+    sub_email = ('id', 'subscriber_email')
+    sub_savebtn = ('id', 'btnSave')
+    sub_cancelbtn = ('id', 'btnCancel')
+    sub_table = ('xpath', '//table[@id="resultTable"]')
+    add_record = ('xpath', '//table[@id="resultTable"]/tbody/tr/td[text()="anne_tang@epam.com"]')
+    record_box = ('xpath', '//tr/td[1]')
+    del_OK = ('xpath', '//input[@id="dialogDeleteBtn"]')
+    del_cancel = ('xpath', '//input[@class="btn reset"][@value="Cancel"]')
+
 
     def __init__(self, browser):
-        super(Admin, self).__init__(browser)
+        super(EmailSub, self).__init__(browser)
 
     # def switch_menu(self):
     #     """
@@ -50,6 +66,7 @@ class EmailSub(Admin):
         self.check_element_selected(self.lea_approvals_box)
         self.check_element_selected(self.lea_cancel_box)
         self.check_element_selected(self.lea_rejections_box)
+        Log.info("enable notification successfully")
 
     def disable_notification(self):
         self.click(self.edit_btn)
@@ -59,5 +76,64 @@ class EmailSub(Admin):
         self.click(self.lea_cancel_box)
         self.click(self.lea_rejections_box)
         self.click(self.save_btn)
+        Log.info("disable notification successfully")
+
+    def add_subscribers_leave_app(self, ptitle1, ptitle2, name, email):
+        self.click(self.lea_app)
+        assert ptitle1 == self.get_element_text(self.sub_title)
+        Log.info("Arrive Subscribers : Leave Applications page")
+        self.click(self.sub_addbtn)
+        assert ptitle2 == self.get_element_text(self.add_subtitle)
+        Log.info("Arrive Add Subscriber page")
+        self.input_text(name, self.sub_name)
+        self.input_text(email, self.sub_email)
+        self.click(self.sub_cancelbtn)
+        self.click(self.sub_addbtn)
+        self.input_text(name, self.sub_name)
+        self.input_text(email, self.sub_email)
+        self.click(self.sub_savebtn)
+        Log.info("add subscribers successfully")
+
+    def validate_add_suc(self, value):
+        # validate add successfully
+        lst_result = self.get_element_text(self.sub_table).encode('utf-8').split('\n')
+        # print lst_result
+        for i in lst_result:
+            if i == value:
+                print 'pass'
+                break
+
+    def delete_record(self):
+        self.click(self.record_box)
+        self.click(self.sub_delbtn)
+        self.sleep(3)
+        self.click(self.del_cancel)
+        self.click(self.sub_delbtn)
+        self.sleep(3)
+        self.click(self.del_OK)
+        self.sleep(2)
+        self.click(self.sub_backbtn)
+        Log.info("delete subscribers successfully")
+
+    def validate_delete(self, value):
+        lst_result = self.get_element_text(self.sub_table).encode('utf-8').split('\n')
+        for i in lst_result:
+            if i == value:
+                print 'failed'
+                break
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
--- pageobjects/admin/config/localization.py
+++ pageobjects/admin/config/localization.py
@@ -1,5 +1,77 @@
-'''
-Created on 2018��4��26��
+# -*- coding: utf-8 -*-
+"""
+Created on 2018��4��26��
 
 @author: Angelia_Yao
-'''
+"""
+
+import random
+from pageobjects.admin.admin import Admin
+from lib.log import Log
+
+
+class Localization(Admin):
+    """
+    Localization components
+    """
+    title = ('xpath', '//h1[text()="Localization"]')
+    language = ('id', 'localization_dafault_language')
+    language_box = ('id', 'localization_use_browser_language')
+    supp_language = ('xpath', '//a[@class="btn1"]')
+    date_format = ('id', 'localization_default_date_format')
+    link = ('xpath', '//a[@href="http://www.orangehrm.com/localization-help.shtml"]')
+    edit_btn = ('xpath', '//input[@value="Edit"]')
+    save_btn = ('xpath', '//input[@value="Save"]')
+    Ok_btn = ('xpath', '//input[@value="Ok"]')
+    link_lang = ('id', 'languageList')
+    link_group = ('id', 'add_label_language_group_id')
+    window_name = 'Orange - Localizit'
+    download_btn = ('id', 'downloadDictionary')
+
+    def __init__(self, browser):
+        super(Localization, self).__init__(browser)
+
+    def select_randm_list(self, keys):
+        value = self.get_element_text(keys).encode('utf-8')
+        value_list = value.rsplit('\n')
+        print value_list
+        length = len(value_list)
+        print length
+        if value_list[0] == '--Select--':
+            num = random.randint(1, length-1)
+        elif value_list[0] == '-- Month --':
+            num = random.randint(1, length - 1)
+        else:
+            num = random.randint(0, length-1)
+        print(num)
+        self.select_option(keys, num)
+
+    def switch_to_tab(self):
+        handles = self.get_all_window()  # 获取所有窗口
+        for handle in handles:  # 切换窗口
+            if handle != self.get_current_window():
+                print 'switch to second window', handle
+                self.switch_to_window(handle)  # 切换到第二个窗口
+
+    def edit_local(self, ptitle):
+        assert ptitle == self.get_element_text(self.title)
+        handle = self.get_current_window()
+        self.click(self.edit_btn)
+        self.select_randm_list(self.language)
+        self.click(self.language_box)
+        self.click(self.supp_language)
+        self.sleep(3)
+        self.click(self.Ok_btn)
+        self.select_randm_list(self.date_format)
+        self.click(self.link)
+        self.switch_to_tab()
+        self.sleep(2)
+        self.close_browser()
+        self.sleep(3)
+        self.switch_to_window(handle)
+        self.click(self.save_btn)
+        Log.info("edit localization successfully")
+
+
+
+
